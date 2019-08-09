d5f3a0763e3bac7b3b7757c2eb39ccefc46cb492
Xiang Wang <ramwin@qq.com>
2019-08-09 16:21:19 +0800

    add requires and version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pillow-snippet",
-    version="0.0.2",
+    version="0.0.3",
     author="Xiang Wang",
     author_email="ramwin@qq.com",
     description="some useful snippet for image",
@@ -17,6 +17,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ramwin/pillow_snippet",
     packages=setuptools.find_packages(),
+    install_requires=[
+        "pillow",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
